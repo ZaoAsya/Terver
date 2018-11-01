705c598c224297d602b3185b26c6fffbfd408e34
Asya Zaostrovskaya <36674295+ZaoAsya@users.noreply.github.com>
2018-11-01 16:35:39 +0500

    fixed bug with correlation

--- Terver.py
+++ Terver.py
@@ -12,10 +12,13 @@ GetValue = {'t_kn302': lambda k, m: (max(k + m, 2 * m), DistrLaw['k'][k] * Distr
 
 
 def get_distribution_law(v):
-    d = {i: 0 for i in range(0, 82)}
-    for k in range(1, 7):
-        for n in range(1, 7):
-            val, p = GetValue[v](k, n)
+    d = {i: 0 for i in range(0, 145)}
+    for k in range(1, 13):
+        for m in range(1, 7):
+            # if v == 't3':
+            #     print(k, m, k*m)
+            if v != 't3' and (k > 6 or m > 6): continue
+            val, p = GetValue[v](k, m)
             d[val] += p
     DistrLaw[v] = d
     return d
@@ -49,16 +52,16 @@ def get_median(distribution):
             return value
 
 
-def get_covariance(t1, t2):
+def get_covariance(math_exp_1, math_exp_2):
     """cov(c1, c2) = E(c1*c2)-E(c1)*E(c2)"""
     t3 = get_distribution_law('t3')
-    return get_mathematical_expectation(t3) \
-           - get_mathematical_expectation(DistrLaw[t1]) * get_mathematical_expectation(DistrLaw[t2])
+    return get_mathematical_expectation(t3) - math_exp_1 * math_exp_2
 
 
-def get_correlation(t1, t2):
+def get_correlation(cov, d1, d2):
     """p(t1, t2) = Cov(t1, t2) / (D(t1)*D(t2))^(1/2)"""
-    return get_covariance(t1, t2) / pow(get_dispersion(DistrLaw[t1]) * get_dispersion(DistrLaw[t2]), 1/2)
+    # return get_covariance(t1, t2) / pow(get_dispersion(DistrLaw[t1]) * get_dispersion(DistrLaw[t2]), 1/2)
+    return cov / pow(d1*d2, 1/2)
 
 
 if __name__ == '__main__':
@@ -68,12 +71,21 @@ if __name__ == '__main__':
     for value in t1.keys():
         if t1[value] != 0:
             print('   ', value, '->', t1[value])
-    print('>>>Mathematical expectation =', get_mathematical_expectation(t1))
-    print('>>>Dispersion =', get_dispersion(t1))
+    e1 = get_mathematical_expectation(t1)
+    print('>>>Mathematical expectation =', e1)
+    d1 = get_dispersion(t1)
+    print('>>>Dispersion =', d1)
     print('>>>Mean square deviation =', get_mean_square_deviation(t1))
     print('>>>Median =', get_median(t1), '\n')
     t2 = get_distribution_law('t_kb301')
-    cov = get_covariance('t_kn302', 't_kb301')
-    print('>>>Covariance between t1 = max(k + m, 2m) and t2 = min(2^k, m) =', cov)
-    p = get_correlation('t_kn302', 't_kb301')
+    print("T2 = min(2^k, m)")
+    print(">>>Distribution law:")
+    for value in t1.keys():
+        if t2[value] != 0:
+            print('   ', value, '->', t2[value])
+    e2 = get_mathematical_expectation(t2)
+    d2 = get_dispersion(t2)
+    cov = get_covariance(e1, e2)
+    print('\n>>>Covariance between t1 = max(k + m, 2m) and t2 = min(2^k, m) =', cov)
+    p = get_correlation(cov, d1, d2)
     print('>>>Correlation between t1 = max(k + m, 2m) and t2 = min(2^k, m) =', p)
